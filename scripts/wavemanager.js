1cf1d5df17fb9a78af5326b1d54b3ccb73065fe8
DuskZhang <rzhang20001121@gmail.com>
2020-03-05 14:13:35 -0700

    o boi

--- scripts/wavemanager.js
+++ scripts/wavemanager.js
@@ -4,236 +4,194 @@
 function decideWave() {
     if (wave == 0) {
         waveOn = true;
-        waveMuscle = 6; // norm 5
-        waveWorkers = 0;
-        waveBoom = 3;
-        waveTeacher = 1;
-     
+        waveMuscle = 6; 
+        waveBoom = 4;
         wave++;
-    } else if (wave == 1) {
+    } else if (wave == 1) { //add teachers
         waveOn = true;
         waveMuscle = 10;
-        waveWorkers = 2;
-        waveBoom = 11;
-        waveTeacher = 1;
+        waveBoom = 10;
+        waveTeacher = 3;
         spawnrate = 54;
         wave++;
-    } else if (wave == 2) {
-        waveOn = true;
-        waveMuscle = 10;
-        waveWorkers = 0;
-        waveBoom = 9;
-        waveImprovedmus = 0;
-        waveTeacher = 4;
-        enemyQueue.push(new Philosopher(0, 0))
+    } else if (wave == 2) { //add workers and philosopher
+        waveOn = true; 
+        waveWorkers = 4;
+        enemyQueue.push(new Philosopher(0, 0));
         spawnrate = 50;
         wave++;
-    } else if (wave == 3) { //john
+    } else if (wave == 3) { //john cena 
         waveOn = true;
-        waveMuscle = 30;
-        waveWorkers = 5;
-        waveBoom = 10;
+        waveWorkers = 10;
         spawnrate = 45;
         enemyQueue.push(new JohnCena(0, 0));
         wave++;
-    } else if (wave == 4) {
-
+    } else if (wave == 4) { //muscles
         waveOn = true;
-        waveMuscle = 44;
-        waveWorkers = 8;
-        waveBoom = 5;
-        waveImprovedmus = 3;
+        waveMuscle = 20;
+        waveImprovedmus = 10;
         spawnrate = 41;
-        waveTeacher = 8;
-        enemyQueue.push(new JohnCena(0, 0));
         enemyQueue.push(new JohnCena(0, 0));
         wave++;
-    } else if (wave == 5) {
+    } else if (wave == 5) { //teachers, boomers, workers
         waveOn = true;
-        waveMuscle = 33;
-        waveBoom = 30;
-        waveTeacher = 29;
-        waveImprovedmus = 30;
+        waveMuscle = 10;
+        waveBoom = 15;
+        waveTeacher = 20;
         waveWorkers = 30
         spawnrate = 35;
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new Philosopher(0, 0))
-        enemyQueue.push(new JohnCena(0, 0));
+        var i;
+        for (i = 0; i < 4; i++) {
+          enemyQueue.push(new Philosopher(0, 0));
+        }
         wave++;
-    } else if (wave == 6) {
+    } else if (wave == 6) { //basic grind wave
         waveOn = true;
-        waveMuscle = 24;
-        waveBoom = 40;
-        waveTeacher = 50;
+        waveMuscle = 0; //removing basic musclemen
         waveImprovedmus = 30;
         waveWorkers = 40
         spawnrate = 33;
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new Philosopher(0, 0))
-        enemyQueue.push(new JohnCena(0, 0));
+        for (i = 0; i < 2; i++) {
+          enemyQueue.push(new Philosopher(0, 0));
+          enemyQueue.push(new JohnCena(0, 0));
+        }
         wave++;
-        //21 waves
     } else if (wave == 7) {
         waveOn = true;
-        waveMuscle = 0;
-        waveBoom = 0;
+        waveBoom = 0; //removing boomers
         waveTeacher = 60;
         waveImprovedmus = 60;
         waveWorkers = 40;
         waveCavalry = 30;
         spawnrate = 30;
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new Philosopher(0, 0))
-        enemyQueue.push(new JohnCena(0, 0));
         wave++;
     } else if (wave == 8) {
         waveOn = true;
-        waveBoom = 5;
-        waveTeacher = 40;
-        waveImprovedmus = 80;
-        waveWorkers = 20
+        waveTeacher = 20;
+        waveImprovedmus = 40;
+        waveWorkers = 20;
         waveCavalry = 60;
         enemyQueue.push(new BehindTheMeme(0, 0));
         spawnrate = 27;
         wave++;
     } else if (wave == 9) {
         waveOn = true;
-        waveBoom = 0;
-        waveTeacher = 50;
-        waveImprovedmus = 80;
-        waveWorkers = 100
+        waveTeacher = 0; //removing teachers
         waveCavalry = 50;
-        enemyQueue.push(new Philosopher(0, 0))
+        for (i = 0; i < 12; i++) {
+            enemyQueue.push(new Philosopher(0, 0));
+        }
         spawnrate = 25;
 
         wave++;
-    } else if (wave == 10) {
+    } else if (wave == 10) { //adding emos
         waveOn = true;
-        waveMuscle = 0;
-        waveBoom = 30;
-        waveTeacher = 50;
         waveImprovedmus = 20;
-        waveWorkers = 20
         spawnrate = 15;
         waveEmo = 25;
-
+        for (i = 0; i < 4; i++) {
+            enemyQueue.push(new Philosopher(0, 0));
+            enemyQueue.push(new JohnCena(0, 0));
+        }
         wave++;
     } else if (wave == 11) {
         waveOn = true;
-        waveBoom = 0;
-        waveImprovedmus = 120;
+        for (i = 0; i < 4; i++) {
+            enemyQueue.push(new Philosopher(0, 0));
+            enemyQueue.push(new JohnCena(0, 0));
+        }
         enemyQueue.push(new Jordan(0, 0));
         spawnrate = 17;
-         waveEmo = 45;
+        waveEmo = 45;
         wave++;
     } else if (wave == 12) {
         waveOn = true;
-        waveTeacher = 30;
         waveImprovedmus = 50;
-        waveWorkers = 20
+        waveWorkers = 0; //removing workers
         spawnrate = 20;
         wave++;
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new Jordan(0, 0));
-        enemyQueue.push(new Jordan(0, 0));
-        //21 waves
+        for (i = 0; i < 3; i++) {
+            enemyQueue.push(new Jordan(0, 0));
+            enemyQueue.push(new Jordan(0, 0));
+            enemyQueue.push(new JohnCena(0, 0));
+            enemyQueue.push(new BehindTheMeme(0, 0));
+        }
     } else if (wave == 13) {
         waveOn = true;
-        waveBoom = 10;
-        waveTeacher = 50;
-        waveImprovedmus = 20;
-        waveWorkers = 20
-        enemyQueue.push(new Philosopher(0, 0));
+        for (i = 0; i < 8; i++) {
+            enemyQueue.push(new BehindTheMeme(0, 0));
+        }
         spawnrate = 15;
         wave++;
     } else if (wave == 14) {
         waveOn = true;
-        waveTeacher = 60;
-        waveImprovedmus = 40;
-        waveWorkers = 40
+        waveImprovedmus = 0; //ending muscles
+        for (i = 0; i < 35; i++) {
+            enemyQueue.push(new JohnCena(0, 0));
+        }
         spawnrate = 18;
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
         wave++
-    } else if (wave == 15) {
+    } else if (wave == 15) { //basic grind wave
         waveOn = true;
-        waveBoom = 12;
-        waveTeacher = 40;
         spawnrate = 17;
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new Philosopher(0, 0));
-        enemyQueue.push(new Philosopher(0, 0));
+        waveCavalry = 30;
+        waveEmo = 30;
+        for (i = 0; i < 15; i++) {
+            enemyQueue.push(new BehindTheMeme(0, 0));
+            enemyQueue.push(new Jordan(0, 0));
+            enemyQueue.push(new JohnCena(0, 0));
+            enemyQueue.push(new Philosopher(0, 0));
+        }
         wave++;
-    } else if (wave == 16) {
+    } else if (wave == 16) { //mega grind wave bring back workers
         waveOn = true;
-
-        waveTeacher = 60;
-        waveImprovedmus = 40;
-        waveWorkers = 40
+        waveCavalry = 10;
+        waveEmo = 10;
+        waveWorkers = 10;
+        waveTeachers = 30
         spawnrate = 16;
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
+        for (i = 0; i < 40; i++) {
+            enemyQueue.push(new BehindTheMeme(0, 0));
+            enemyQueue.push(new Jordan(0, 0));
+            enemyQueue.push(new JohnCena(0, 0));
+            enemyQueue.push(new Philosopher(0, 0));
+        }
         wave++;
-    } else if (wave == 17) {
+    } else if (wave == 17) { //everyone comes back this wave
         waveOn = true;
-        waveTeacher = 10;
-        waveImprovedmus = 100;
-        waveWorkers = 120
-        spawnrate = 13;
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        wave++;
+        waveCavalry = 20;
+        waveEmo = 20;
+        waveWorkers = 10;
+        waveTeachers = 30
+        waveMuscle = 10
+        waveBoom = 10
+        waveImprovedmus = 10
+        spawnrate = 14;
+        for (i = 0; i < 60; i++) {
+            enemyQueue.push(new BehindTheMeme(0, 0));
+            enemyQueue.push(new Jordan(0, 0));
+            enemyQueue.push(new JohnCena(0, 0));
+            enemyQueue.push(new Philosopher(0, 0));
+        }
+        wave++
     } else if (wave == 18) {
         waveOn = true;
-         waveEmo = 125;
-        waveTeacher = 60;
-        waveImprovedmus = 40;
-        waveWorkers = 40
-        spawnrate = 10;
-        wave++;
-    } else if (wave == 19) {
-        waveOn = true;
-
-        waveBoom = 20;
-        waveTeacher = 70;
-        waveImprovedmus = 50;
-        waveWorkers = 40
+        waveCavalry = 20;
+        waveEmo = 20;
+        waveWorkers = 10;
+        waveTeachers = 30
+        waveMuscle = 10
+        waveBoom = 10
+        waveImprovedmus = 10
         spawnrate = 8;
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
+        for (i = 0; i < 60; i++) {
+            enemyQueue.push(new BehindTheMeme(0, 0));
+            enemyQueue.push(new Jordan(0, 0));
+            enemyQueue.push(new JohnCena(0, 0));
+            enemyQueue.push(new Philosopher(0, 0));
+        }
         wave++
-    } else if (wave == 20) {
-        waveOn = true;
-        waveTeacher = 100;
-        waveImprovedmus = 70;
-        waveWorkers = 9;
-        spawnrate = 7;
-        enemyQueue.push(new Philosopher(0, 0));
-        enemyQueue.push(new Philosopher(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new JohnCena(0, 0));
-        enemyQueue.push(new BehindTheMeme(0, 0));
         enemyQueue.push(new Trump(0, 0));
-        wave++;
     }
 
     buildWave();
